7683a549efdbf3adfc389a99894886653d8da45e
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 02:48:50 +0600

    first commit

---
+++ stringDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sattar";
+           
+            Console.WriteLine(name);
+            Console.WriteLine(name[5]);
+
+            //string lines = "line 1\nline2";
+            string lines = "word 1\tword2";
+            string directory = "C:\\Program\\C#";
+            string myBook = "\"Golpe Golpe C Programming\" is an awesome book in C";
+            Console.WriteLine(myBook);
+        }
+    }
+}
